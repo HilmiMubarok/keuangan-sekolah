8da3aa471b0d422d566b3cb3749d73230bf3d449
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-02-16 15:24:25 +0700

    add kelas views and kelas model

---
+++ application/models/KelasModel.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class KelasModel extends CI_Model
+{
+
+    public function get()
+    {
+        return $this->db->get("kelas")->result();
+    }
+
+    public function get_by($where)
+    {
+        return $this->db->get_where("kelas", $where)->row();
+    }
+
+    public function save($data)
+    {
+        return $this->db->insert("kelas", $data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->db->update("kelas", $data, $id);
+    }
+
+    public function delete($id)
+    {
+        return $this->db->delete("kelas", $id);
+    }
+}
+
+
+?>
