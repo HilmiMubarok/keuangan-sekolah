0c9cf9f88bddbe7399fc8bddc1e1a8d46e8967df
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-02-18 13:54:52 +0700

    add siswa view index

---
+++ application/controllers/Siswa.php
@@ -0,0 +1,28 @@
+<?php
+
+class Siswa extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // $this->load->model('KelasModel');
+        // $this->load->model('JurusanModel');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Siswa';
+        $data['jabatan']   = $this->session->userdata('role');
+        $data['nama_user'] = $this->session->userdata('name');
+        // $data['kelas'] = $this->KelasModel->get();
+        // $data['jurusan'] = $this->JurusanModel->get();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('siswa/index', $data);
+        $this->load->view('templates/footer', $data);
+    }
+
+}
+
+
+?>
---
+++ application/views/siswa/index.php
@@ -0,0 +1,23 @@
+<div class="container-fluid">
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Siswa</li>
+        </ol>
+    </nav>
+	
+	<?php if ($this->session->flashdata()): ?>
+		<div class="alert alert-dismissible fade show alert-<?= $this->session->flashdata('icon') ?> bg-<?= $this->session->flashdata('icon') ?> text-white">
+			<?= $this->session->flashdata('pesan') ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true" class="text-white">&times;</span>
+            </button>
+		</div>
+	<?php endif ?>
+
+    
+    
+	
+	
+</div>
