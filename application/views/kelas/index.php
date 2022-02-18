76d4af6c3e416b64ee8a68609b7244b9ffb999fb
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-02-18 13:50:50 +0700

    add alert close button kelas index

--- application/views/kelas/index.php
+++ application/views/kelas/index.php
@@ -8,8 +8,11 @@
     </nav>
 	
 	<?php if ($this->session->flashdata()): ?>
-		<div class="alert alert-<?= $this->session->flashdata('icon') ?> bg-<?= $this->session->flashdata('icon') ?> text-white">
+		<div class="alert alert-dismissible fade show alert-<?= $this->session->flashdata('icon') ?> bg-<?= $this->session->flashdata('icon') ?> text-white">
 			<?= $this->session->flashdata('pesan') ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true" class="text-white">&times;</span>
+            </button>
 		</div>
 	<?php endif ?>
 
