f14987df98d636f5ce5468634023442520fc8f36
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-02-18 13:40:29 +0700

    add breadcrumb jurusan index

--- application/views/jurusan/index.php
+++ application/views/jurusan/index.php
@@ -1,4 +1,11 @@
 <div class="container-fluid">
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Jurusan</li>
+        </ol>
+    </nav>
 	
 	<?php if ($this->session->flashdata()): ?>
 		<div class="alert alert-<?= $this->session->flashdata('icon') ?> bg-<?= $this->session->flashdata('icon') ?> text-white">
