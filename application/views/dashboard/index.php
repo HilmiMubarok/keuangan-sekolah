d863ac69d0e0938ac01407982b3d2b5528e0640c
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-08 12:39:01 +0700

    fix dashboard index link

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -53,69 +53,75 @@
     </div>
 
     <div class="col-xl-4 col-md-4 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              Jumlah Siswa
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                    <?= $jumlah_siswa ?>
+      <a href="<?= base_url('siswa') ?>" class="text-secondary">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                Jumlah Siswa
+                <div class="row no-gutters align-items-center">
+                  <div class="col-auto">
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                      <?= $jumlah_siswa ?>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              <div class="col-auto">
+                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
 
     <div class="col-xl-4 col-md-4 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              Jumlah Kelas
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                    <?= $jumlah_kelas ?>
+      <a href="<?= base_url('kelas') ?>" class="text-secondary">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                Jumlah Kelas
+                <div class="row no-gutters align-items-center">
+                  <div class="col-auto">
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                      <?= $jumlah_kelas ?>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              <div class="col-auto">
+                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
 
     <div class="col-xl-4 col-md-4 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              Jumlah Jurusan
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                    <?= $jumlah_jurusan ?>
+      <a href="<?= base_url('jurusan') ?>" class="text-secondary">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                Jumlah Jurusan
+                <div class="row no-gutters align-items-center">
+                  <div class="col-auto">
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                      <?= $jumlah_jurusan ?>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              <div class="col-auto">
+                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
 
 
