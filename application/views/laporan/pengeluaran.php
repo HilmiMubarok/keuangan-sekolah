2c132bef33ee388afbd32b84596cd9b029626a2d
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-12 02:01:01 +0700

    update

--- application/views/laporan/pengeluaran.php
+++ application/views/laporan/pengeluaran.php
@@ -38,6 +38,46 @@
         </div>
 
     </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card shadow mb-3">
+                <div class="card-header bg-primary text-white">
+                    <h5 class="card-title">Data Pengeluaran</h5>
+                </div>
+                <div class="card-body">
+                    <a href="" class="mb-3 btn btn-primary text-white">
+                        <i class="fas fa-print"></i> Cetak Semua
+                    </a>
+                    <table id="dataTable" class="table table-hover table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>Kategori</th>
+                                <th>Nominal</th>
+                                <th>Keterangan</th>
+                                <th>Penginput</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1; foreach($pengeluaran as $p): ?>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= formatHariTanggal($p->tanggal) ?></td>
+                                    <td><?= $p->nama_jenis_pengeluaran ?></td>
+                                    <td><?= $p->nominal ?></td>
+                                    <td><?= $p->keterangan ?></td>
+                                    <td><?= $p->user_name ?></td>
+                                </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div> <!-- End Card -->
+        </div>
+
+    </div>
 	
 	
 </div>
