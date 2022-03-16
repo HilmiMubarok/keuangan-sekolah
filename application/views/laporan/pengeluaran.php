c8ba33a78163e73b7e364a54a4f01142926db3ac
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2022-03-16 20:51:31 +0700

    add laporan pengeluaran view

--- application/views/laporan/pengeluaran.php
+++ application/views/laporan/pengeluaran.php
@@ -24,13 +24,65 @@
                     <h5 class="card-title">Laporan Pengeluaran</h5>
                 </div>
                 <div class="card-body">
-                    <form action="<?= base_url("kelas/simpan") ?>" method="POST">
-                        <div class="form-group">
-                            <label for="">Nama Kelas</label>
-                            <input type="text" name="nama_kelas" placeholder="Masukkan Nama Kelas" class="form-control">
+                    <form action="<?= base_url("cetak/aktiva_tetap") ?>" method="GET">
+
+                        <div class="row">
+                            <div class="col-12 col-lg-4">
+                                <div class="form-group p-2 mr-3" style="width:100%; border: 1px solid grey; border-radius: 10px;">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="jenis" id="exampleRadios1" value="all">
+                                        <label class="form-check-label" for="exampleRadios1">
+                                            Semua Data
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <div class="form-group p-2 mr-3" style="width:100%; border: 1px solid grey; border-radius: 10px;">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="jenis" id="exampleRadios2" value="bulan">
+                                        <label class="form-check-label" for="exampleRadios2">
+                                            Per Bulan
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <select name="bulan" class="form-control">
+                                        <option value="1">Januari</option>
+                                        <option value="2">Februari</option>
+                                        <option value="3">Maret</option>
+                                        <option value="4">April</option>
+                                        <option value="5">Mei</option>
+                                        <option value="6">Juni</option>
+                                        <option value="7">Juli</option>
+                                        <option value="8">Agustus</option>
+                                        <option value="9">September</option>
+                                        <option value="10">Oktober</option>
+                                        <option value="11">November</option>
+                                        <option value="12">Desember</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <div class="form-group p-2" style="width:100%; border: 1px solid grey; border-radius: 10px;">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="jenis" id="exampleRadios3" value="periode">
+                                        <label class="form-check-label" for="exampleRadios3">
+                                            Per Periode
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="date" class="form-control" name="tgl_start" placeholder="Dari">
+                                </div>
+                                <div class="form-group">
+                                    <input type="date" class="form-control" name="tgl_end" placeholder="Sampai">
+                                </div>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btn-primary mb-3">
-                            <i class="fas fa-save"></i> Simpan
+                        
+                        <button type="submit" class="btn btn-primary" name="cetak" value="cetak">
+                            <i class="fas fa-print"></i> Cetak
                         </button>
                     </form>
                 </div>
